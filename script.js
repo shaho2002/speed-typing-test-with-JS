41afdc0e94bb6e10020aa66a20ecef524a6b4aaf
shaho2002 <mrshahoazad81@gmail.com>
2026-06-08 15:21:39 +0330

    Add updateAccuracyPercent function

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let typingInput = document.querySelector('#typingInput');
 let textDisplay = document.querySelector('.text-display');
 let resetBtn = document.querySelector('#resetBtn');
 let showMistake = document.querySelector('#showMistake');
+let accuracy = document.querySelector('#accuracy');
+
 
 let totalMs = 0;
 let minutes;
@@ -12,12 +14,15 @@ let centiSeconds;
 let pressFirstKey = false;
 
 let mistake = 0;
+let totalTyped = 0;
+let accuracyPercent = 0;
 function addPrefixZero(time) {
     if (time <= 9) {
         time = '0' + time;
     }
     return time;
 }
+
 
 function start() {
 
@@ -30,6 +35,7 @@ function start() {
 }
 typingInput.addEventListener('keypress', start);
 
+
 function runTimer() {
     totalMs = totalMs + 10;
 
@@ -39,10 +45,22 @@ function runTimer() {
 
     let currentTime = addPrefixZero(minutes) + ':' + addPrefixZero(seconds) + ':' + addPrefixZero(centiSeconds);
     theTimer.innerHTML = currentTime;
-    showMistake.innerHTML = mistake;        
-
+    showMistake.innerHTML = mistake;
+accuracy.innerHTML = accuracyPercent.toFixed(0) + '%';
 
 }
+
+
+function updateAccuracyPercent() {
+    if (totalTyped == 0) {
+        accuracyPercent = 0;
+    } else {
+        let correctChars = totalTyped - mistake;
+        correctChars = (correctChars < 0) ? 0 : correctChars;
+        accuracyPercent = (correctChars / totalTyped) * 100;
+    }
+}
+
 
 function spellCheck() {
     let originText = textDisplay.textContent.trim();
@@ -54,13 +72,23 @@ function spellCheck() {
     }
     else if (originText.startsWith(userText)) {
         typingInput.style.color = "green";
+
     }
     else {
         typingInput.style.color = "red";
         mistake++;
     }
+    totalTyped++;
+
+    updateAccuracyPercent();
+
 }
-typingInput.addEventListener("keyup", spellCheck);
+typingInput.addEventListener("keyup", (event) => {
+    if (event.key.length === 1) {
+        spellCheck();
+    }
+});
+
 
 function reset() {
 
@@ -70,7 +98,11 @@ function reset() {
     seconds = 0;
     centiSeconds = 0;
     mistake = 0;
-    showMistake.innerHTML = '0';
+    totalTyped = 0;
+    mistake = 0;
+    accuracyPercent = 0;
+    accuracy.innerHTML = "0";
+    showMistake.innerHTML = "0";
     interval = null;
     theTimer.innerHTML = "00:00:00";
     typingInput.value = "";
